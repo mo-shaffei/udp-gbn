096532fb76b8d5e40ef3ae6308122bf6f032bb71
mo-shaffei <s-mohamed_chaffei@zewailcity.edu.eg>
2022-04-22 20:14:50 +0200

    Added context manager

--- sender.py
+++ sender.py
@@ -1,10 +1,9 @@
 import socket
 
 server_port = 12000
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(('', server_port))
-print("Waiting for request")
-message, client_address = server_socket.recvfrom(2048)
-print("Request received from ", client_address)
-server_socket.sendto("Received".encode(), client_address)
-server_socket.close()
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
+    server_socket.bind(('', server_port))
+    print("Waiting for request")
+    message, client_address = server_socket.recvfrom(2048)
+    print("Request received from ", client_address)
+    server_socket.sendto("Received".encode(), client_address)
