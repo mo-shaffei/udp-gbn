1fc398ca01383bc5efe5db2e04ea7ba66d1dec34
mo-shaffei <s-mohamed_chaffei@zewailcity.edu.eg>
2022-04-23 00:35:51 +0200

    Divide file into chunks

--- receiver.py
+++ receiver.py
@@ -1,8 +1,50 @@
 import socket
+import sys
 
-server_name = "localhost"
-server_port = 12000
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
-    client_socket.sendto("hi".encode(), (server_name, server_port))
-    received_msg, server_address = client_socket.recvfrom(2048)
-    print(received_msg.decode())
+
+class Receiver:
+    """
+    Class to implement a sender
+
+    """
+
+    def __init__(self, max_seg_size: int, receiver_port: int = 12001):
+        self.max_seg_size = max_seg_size  # store maximum segment size
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create UDP socket
+        self.socket.bind(('', receiver_port))  # bind socket to the receiver port
+
+    def receive(self, filename: str) -> None:
+        """
+        Receive file from sender
+        :param filename: name of received file
+        :return: None
+        """
+        received_data = bytearray()  # init received_data to empty bytearray
+        while 1:
+            # receive a packet, set buffer size to max_seg_size + header size
+            packet, server_address = self.socket.recvfrom(self.max_seg_size + 6)
+            packet_id = packet[:2]  # extract packet_id from first 16 bits
+            print("Received packet ", int.from_bytes(packet_id, sys.byteorder))
+            file_id = packet_id[2:4]  # extract file_id from second 16 bits
+            received_data.extend(packet[4:-2])  # extract payload and append it to received_data
+            trailer_bits = packet[-2:]  # extract trailer bits from last 16 bits
+            if trailer_bits.hex() == 'ffff':  # if trailer_bits is 'ffff' then all data is received
+                break
+
+        print("Received all packets")
+        with open(filename, 'wb') as file:  # write received data to file
+            file.write(received_data)
+        print("File written at ", filename)
+
+    def close_socket(self) -> None:
+        self.socket.close()
+
+
+def main():
+    receiver = Receiver(max_seg_size=2048)
+    receiver.receive("Received Files/SmallFile.png")
+    receiver.close_socket()
+
+
+if __name__ == "__main__":
+    main()
--- sender.py
+++ sender.py
@@ -1,9 +1,55 @@
 import socket
+import sys
 
-server_port = 12000
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
-    server_socket.bind(('', server_port))
-    print("Waiting for request")
-    message, client_address = server_socket.recvfrom(2048)
-    print("Request received from ", client_address)
-    server_socket.sendto("Received".encode(), client_address)
+
+class Sender:
+    """
+    Class to implement a sender
+    """
+
+    def __init__(self, rec_ip: str, rec_port: int, max_seg_size: int, sender_port: int = 12000):
+        self.rec_ip = rec_ip  # store receiver ip
+        self.rec_port = rec_port  # store receiver port
+        self.max_seg_size = max_seg_size  # store maximum segment size
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create UDP socket
+        self.socket.bind(('', sender_port))  # bind socket to the sender port
+
+    def send(self, filename: str) -> None:
+        """
+        Send file to the receiver by dividing it into ceil(file_len/max_seg_size) segments
+        and append headers to each segment
+        :param filename: path and name of file to send
+        :return: None
+        """
+        with open(filename, 'rb') as file:  # read bytes from file
+            image = file.read()
+
+        if len(image) // self.max_seg_size > (2 ** 16) - 1:  # ensure packet_id won't overflow
+            raise Exception('image size and mss overflows 16 bits packet id')
+
+        packet_id = 0  # init packet_id
+        file_id = 0  # init file_id
+        for i in range(0, len(image), self.max_seg_size):  # get max_seg_size bytes from file at a time
+            packet = packet_id.to_bytes(2, sys.byteorder)  # create packet and add 16 bits packet_id
+            packet += file_id.to_bytes(2, sys.byteorder)  # add 16 bits file_id to the packet
+            packet += image[i: i + self.max_seg_size]  # add payload to the packet
+            trailer = "0000" if i + self.max_seg_size < len(image) else "FFFF"  # if last segment make trailer "ffff"
+            packet += bytearray.fromhex(trailer)  # add trailer bits to the packet
+            self.socket.sendto(packet, (self.rec_ip, self.rec_port))  # send packet to receiver
+            print("Sent packet ", packet_id)
+            packet_id += 1  # increment packet_id
+
+        print("Sent all packets")
+
+    def close_socket(self) -> None:
+        self.socket.close()
+
+
+def main():
+    sender = Sender(rec_ip='localhost', rec_port=12001, max_seg_size=2048)
+    sender.send('Test Files/SmallFile.png')
+    sender.close_socket()
+
+
+if __name__ == '__main__':
+    main()
