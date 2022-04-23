d4c8cf99cec8675eb427d2ada4db08df19dc4cb2
mo-shaffei <s-mohamed_chaffei@zewailcity.edu.eg>
2022-04-23 04:02:58 +0200

    Changed members to private

--- receiver.py
+++ receiver.py
@@ -9,10 +9,10 @@ class Receiver:
     """
 
     def __init__(self, max_seg_size: int, receiver_port: int = 12001):
-        self.max_seg_size = max_seg_size  # store maximum segment size
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create UDP socket
-        self.socket.bind(('', receiver_port))  # bind socket to the receiver port
-        self.expectedseqnum = 0
+        self._max_seg_size = max_seg_size  # store maximum segment size
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create UDP socket
+        self._socket.bind(('', receiver_port))  # bind socket to the receiver port
+        self._expectedseqnum = 0
 
     def receive_file(self, filename: str) -> None:
         """
@@ -23,17 +23,17 @@ class Receiver:
         received_data = bytearray()  # init received_data to empty bytearray
         while 1:  # loop until all packets have been received
             # receive a packet, set buffer size to max_seg_size + header size
-            packet, sender_address = self.socket.recvfrom(self.max_seg_size + 6)
+            packet, sender_address = self._socket.recvfrom(self._max_seg_size + 6)
             packet_id = int.from_bytes(packet[:2], sys.byteorder)  # extract packet_id from first 16 bits
             file_id = packet[2:4]  # extract file_id from second 16 bits
-            if packet_id == self.expectedseqnum:  # if packet id matched expectedseqnum, then accept it
+            if packet_id == self._expectedseqnum:  # if packet id matched expectedseqnum, then accept it
                 print("Received packet ", packet_id)
                 self._send_ack(sender_address, file_id)  # send ACK to sender
                 received_data.extend(packet[4:-2])  # extract payload and append it to received_data
                 trailer_bits = packet[-2:]  # extract trailer bits from last 16 bits
                 if trailer_bits.hex() == 'ffff':  # if trailer_bits is 'ffff' then all data is received
                     break
-                self.expectedseqnum += 1  # increment expectedseqnum
+                self._expectedseqnum += 1  # increment expectedseqnum
             else:  # received unexpected packet, discard it
                 print("Discarding packet ", packet_id)
                 self._send_ack(sender_address, file_id)
@@ -50,12 +50,12 @@ class Receiver:
         :param file_id: id of current file being transmitted
         :return:
         """
-        packet = self.expectedseqnum.to_bytes(2, sys.byteorder)  # create ack packet and add expectedseqnum to it
+        packet = self._expectedseqnum.to_bytes(2, sys.byteorder)  # create ack packet and add expectedseqnum to it
         packet += file_id  # add file id to packet
-        self.socket.sendto(packet, sender_address)  # send packet
+        self._socket.sendto(packet, sender_address)  # send packet
 
     def close_socket(self) -> None:
-        self.socket.close()
+        self._socket.close()
 
 
 def main():
--- sender.py
+++ sender.py
@@ -20,15 +20,15 @@ class Sender:
         :param win_size: Go-Back-N window size
         :param timeout: ACK timeout interval before retransmission
         """
-        self.rec_ip = rec_ip  # store receiver ip
-        self.rec_port = rec_port  # store receiver port
-        self.max_seg_size = max_seg_size  # store maximum segment size
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create UDP socket
-        self.socket.bind(('', sender_port))  # bind socket to the sender port
-        self.win_size = win_size  # store Go-Back-N window size
-        self.timeout = timeout  # store ACK timeout interval
+        self._rec_ip = rec_ip  # store receiver ip
+        self._rec_port = rec_port  # store receiver port
+        self._max_seg_size = max_seg_size  # store maximum segment size
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # create UDP socket
+        self._socket.bind(('', sender_port))  # bind socket to the sender port
+        self._win_size = win_size  # store Go-Back-N window size
+        self._timeout = timeout  # store ACK timeout interval
         signal.signal(signal.SIGALRM, self._timeout_handler)  # register timeout function handler
-        self.base = self.nextseqnum = -1  # init base and nextseqnum to -1
+        self._base = self._nextseqnum = -1  # init base and nextseqnum to -1
 
     def send_file(self, filename):
         """
@@ -37,14 +37,14 @@ class Sender:
         :return: None
         """
         self._read_file(filename)  # read file and store it in self.file
-        self.base = self.nextseqnum = 0  # init base and nextseqnum to 0
-        last_packet_id = math.ceil(len(self.file) / self.max_seg_size) - 1  # calculate and store id of last packet
-        while self.base != last_packet_id:  # loop until all packets are transmitted and acknowledged
-            if self.nextseqnum < self.base + self.win_size:  # if new packets can be transmitted in window
-                self._send_packet(self.nextseqnum)  # transmit the packet with id nextseqnum
-                if self.base == self.nextseqnum:  # if first packet in window then start timer
-                    signal.setitimer(signal.ITIMER_REAL, self.timeout)
-                self.nextseqnum += 1  # increment nextseqnum
+        self._base = self._nextseqnum = 0  # init base and nextseqnum to 0
+        last_packet_id = math.ceil(len(self.file) / self._max_seg_size) - 1  # calculate and store id of last packet
+        while self._base != last_packet_id:  # loop until all packets are transmitted and acknowledged
+            if self._nextseqnum < self._base + self._win_size:  # if new packets can be transmitted in window
+                self._send_packet(self._nextseqnum)  # transmit the packet with id nextseqnum
+                if self._base == self._nextseqnum:  # if first packet in window then start timer
+                    signal.setitimer(signal.ITIMER_REAL, self._timeout)
+                self._nextseqnum += 1  # increment nextseqnum
             else:  # no packets can be transmitted in window
                 self._recv_ack()  # wait for acknowledgement from receiver
 
@@ -57,7 +57,7 @@ class Sender:
         with open(filename, 'rb') as file:  # read bytes from file
             self.file = file.read()
 
-        if len(self.file) // self.max_seg_size > (2 ** 16) - 1:  # ensure packet_id won't overflow
+        if len(self.file) // self._max_seg_size > (2 ** 16) - 1:  # ensure packet_id won't overflow
             raise Exception('file size and mss overflows 16 bits packet id')
 
     def _send_packet(self, packet_id: int):
@@ -68,27 +68,27 @@ class Sender:
         """
         print("Sending packet ", packet_id)
         file_id = 0  # set file_id to 0
-        current_byte = packet_id * self.max_seg_size  # get first byte to be sent
-        trailer = "0000" if current_byte + self.max_seg_size < len(
+        current_byte = packet_id * self._max_seg_size  # get first byte to be sent
+        trailer = "0000" if current_byte + self._max_seg_size < len(
             self.file) else "FFFF"  # if last segment make trailer "ffff"
         packet = packet_id.to_bytes(2, sys.byteorder)  # create packet and add 16 bits packet_id
         packet += file_id.to_bytes(2, sys.byteorder)  # add 16 bits file_id to the packet
-        packet += self.file[current_byte: current_byte + self.max_seg_size]  # add payload to the packet
+        packet += self.file[current_byte: current_byte + self._max_seg_size]  # add payload to the packet
         packet += bytearray.fromhex(trailer)  # add trailer bits to the packet
-        self.socket.sendto(packet, (self.rec_ip, self.rec_port))  # send packet to receiver
+        self._socket.sendto(packet, (self._rec_ip, self._rec_port))  # send packet to receiver
 
     def _recv_ack(self):
         """
         Wait to receive acknowledgement from receiver
         :return: None
         """
-        ack_packet, _ = self.socket.recvfrom(4)  # wait for acknowledgement with buffer 4 bytes(2 packet_id, 2 file_id)
+        ack_packet, _ = self._socket.recvfrom(4)  # wait for acknowledgement with buffer 4 bytes(2 packet_id, 2 file_id)
         print("Received ACK ", int.from_bytes(ack_packet[:2], sys.byteorder))
-        self.base = int.from_bytes(ack_packet[:2], sys.byteorder)  # update base to last acknowledged packet id
-        if self.base == self.nextseqnum:  # if last packet in window then stop timer
+        self._base = int.from_bytes(ack_packet[:2], sys.byteorder)  # update base to last acknowledged packet id
+        if self._base == self._nextseqnum:  # if last packet in window then stop timer
             signal.setitimer(signal.ITIMER_REAL, 0)
         else:  # restart timer
-            signal.setitimer(signal.ITIMER_REAL, self.timeout)
+            signal.setitimer(signal.ITIMER_REAL, self._timeout)
 
     def _timeout_handler(self, signum, frame):
         """
@@ -96,12 +96,12 @@ class Sender:
         :return: None
         """
         print("Timeout! Retransmitting...")
-        signal.setitimer(signal.ITIMER_REAL, self.timeout)  # start a new time
-        for packet_id in range(self.base, self.nextseqnum):  # retransmit all packets in window
+        signal.setitimer(signal.ITIMER_REAL, self._timeout)  # start a new time
+        for packet_id in range(self._base, self._nextseqnum):  # retransmit all packets in window
             self._send_packet(packet_id)
 
     def close_socket(self) -> None:
-        self.socket.close()
+        self._socket.close()
 
 
 def main():
